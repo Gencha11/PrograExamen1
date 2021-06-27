21dc87277aeba70b1d73e0fc78c4b987e600ac32
GENESIS DAYANNA CHAVARRIA <gchavarriat0654@uvirtual.ac.cr>
2021-06-27 15:57:47 -0600

    Actualización

---
+++ Entity/dbo/TitulosEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class TitulosEntity
+    {
+        public int? Id_Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+    }
+}
---
+++ WBL/DepartamentosService.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BD;
+using Entity;
+
+namespace WBL
+{
+    public interface IDepartamentosService
+    {
+        Task<DBEntity> Create(DepartamentosEntity entity);
+        Task<DBEntity> Delete(DepartamentosEntity entity);
+        Task<IEnumerable<DepartamentosEntity>> Get();
+        Task<DepartamentosEntity> GetById(DepartamentosEntity entity);
+        Task<DBEntity> Update(DepartamentosEntity entity);
+    }
+
+    public class DepartamentosService : IDepartamentosService
+    {
+        private readonly IDataAccess sql;
+
+        public DepartamentosService(IDataAccess _sql)
+        {
+            sql = _sql;
+        }
+
+        public async Task<IEnumerable<DepartamentosEntity>> Get()
+        {
+            try
+            {
+                var result = sql.QueryAsync<DepartamentosEntity>("DepartamentosObtener");
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<DepartamentosEntity> GetById(DepartamentosEntity entity)
+        {
+            try
+            {
+                var result = sql.QueryFisrtAsync<DepartamentosEntity>("DepartamentosObtener", new
+                {
+                    entity.Id_Departamento
+                });
+
+                return await result;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<DBEntity> Create(DepartamentosEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("DepartamentosInsertar", new
+                {
+                    entity.Descripcion,
+                    entity.Ubicacion,
+                    entity.Estado
+                });
+
+                return await result;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<DBEntity> Update(DepartamentosEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("DepartamentosActualizar", new
+                {
+                    entity.Descripcion,
+                    entity.Ubicacion,
+                    entity.Estado
+                });
+
+                return await result;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<DBEntity> Delete(DepartamentosEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("DepartamentosEliminar", new
+                {
+                    entity.Id_Departamento
+                });
+
+                return await result;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
